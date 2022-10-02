d951127480457d33edcf070ae7c165a503ac2808
Qiushichen-CSCB07 <114188444+Qiushichen-CSCB07@users.noreply.github.com>
2022-10-02 14:29:18 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,13 +1,13 @@
-
-
 public class Driver { 
 	public static void main(String [] args) { 
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5}; 
-		Polynomial p1 = new Polynomial(c1); 
-		double [] c2 = {0,-2,0,0,-9}; 
-		Polynomial p2 = new Polynomial(c2); 
+		double [] c1Coefficient = {6,5};
+		int[] c1Degree = {0,3};
+		Polynomial p1 = new Polynomial(c1Coefficient,c1Degree); 
+		double [] c2Coefficient = {-2, -9};
+		int[] c2Degree = {1,4};
+		Polynomial p2 = new Polynomial(c2Coefficient,c2Degree); 
 		Polynomial s = p1.add(p2); 
 		System.out.println("s(0.1) = " + s.evaluate(0.1)); 
 		if(s.hasRoot(1)) 
